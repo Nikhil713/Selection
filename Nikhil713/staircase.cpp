e75842f0992534bffff997ccdbfe97003729371a
Nikhil713 <31701437+Nikhil713@users.noreply.github.com>
2018-01-11 05:38:58 +0000

    Create staircase.cpp

---
+++ Nikhil713/staircase.cpp
@@ -0,0 +1,21 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main(){
+    int n,i,j;
+    cin >> n;
+    for(i=1;i<=n;i++)
+    {
+        for(j=0;j<n-i;j++)
+            cout<<" ";
+        for(j=0;j<i;j++)
+            cout<<"#";
+     cout<<"\n";
+    }
+    return 0;
+}
