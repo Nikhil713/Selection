6b55727ba621e1dec88c600fa3237fdb4f71eea6
Nikhil713 <31701437+Nikhil713@users.noreply.github.com>
2018-01-11 05:36:22 +0000

    Create compareTriplets.cpp

---
+++ Nikhil713/compareTriplets.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solve(int a0, int a1, int a2, int b0, int b1, int b2){
+    {
+        int a=0,b=0;
+        if(a0>b0)
+            a++;
+        else if(a0<b0)
+            b++;
+        if(a1>b1)
+            a++;
+        else if(a1<b1)
+            b++;
+        if(a2>b2)
+            a++;
+        else if(a2<b2)
+            b++;
+       int ar=10*a+b;
+        return(ar);
+    }   // Complete this function
+}
+
+int main() {
+    int a0;
+    int a1;
+    int a2;
+    cin >> a0 >> a1 >> a2;
+    int b0;
+    int b1;
+    int b2;
+    cin >> b0 >> b1 >> b2;
+    int result = solve(a0, a1, a2, b0, b1, b2);
+    cout<<result/10<<" "<<result%10;
+    cout << endl;
+    
+
+    return 0;
+}
